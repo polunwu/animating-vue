950958a029a3c24ab305f659ffa62d21bcc8722e
polunwu <polunwu@gmail.com>
2020-07-02 00:28:36 +0800

    Add Javascript Hook starter files

--- src/router.js
+++ src/router.js
@@ -21,6 +21,12 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ './views/List.vue')
+    },
+    {
+      path: '/drawer',
+      name: 'drawer',
+      component: () =>
+        import(/* webpackChunkName: "list" */ './views/Drawer.vue')
     }
   ]
 })
